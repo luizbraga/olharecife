f3eaf5d6607dbd9f5c6fec1d78a657c23c8eef43
Luiz Braga <luizbragaa@gmail.com>
2015-05-11 13:11:59 -0300

    Primeiro commit

---
+++ platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/luizbraga/Documents/workspaceWeb/olhaRecife/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
+/Users/luizbraga/Documents/workspaceWeb/olhaRecife/platforms/android/ant-build/HelloWorld.ap_ \
+/Users/luizbraga/Documents/workspaceWeb/olhaRecife/platforms/android/ant-build/classes.dex \
+/Users/luizbraga/Documents/workspaceWeb/olhaRecife/platforms/android/CordovaLib/ant-build/classes.jar \

d3afeab317c249598210d9efd2351c3c50531523
Luiz Braga <luizbragaa@gmail.com>
2015-05-17 18:32:40 -0300

    Adicionado efeito nos botões

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,5 @@
+var roteirosNomes = [];
+
 $(document).ready(function(){
 
 
@@ -5,20 +7,49 @@ $(document).ready(function(){
         $('.search').toggleClass('expanded');
     });
 
+    $('#btn-pe').click(function(){
+        $( "#btn-carro" ).toggle(300);
+        $( "#btn-rio" ).toggle(300);
+        $( "#btn-onibus" ).toggle(300);
+    });
+    $('#btn-carro').click(function(){
+        $( "#btn-pe" ).toggle(300);
+        $( "#btn-rio" ).toggle(300);
+        $( "#btn-onibus" ).toggle(300);
+    });
+    $('#btn-rio').click(function(){
+        $( "#btn-carro" ).toggle(300);
+        $( "#btn-pe" ).toggle(300);
+        $( "#btn-onibus" ).toggle(300);
+    });
+    $('#btn-onibus').click(function(){
+        $( "#btn-carro" ).toggle(300);
+        $( "#btn-rio" ).toggle(300);
+        $( "#btn-pe" ).toggle(300);
+    });
+
+    $('input.typeahead').typeahead(null, {
+		displayKey: 'num',
+        source: numbers.ttAdapter()
+	});
+
+
 });
 
 
+
 function initialize() {
+    var markers = [];
     var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
-          center: new google.maps.LatLng(-8.053334, -34.881429),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-    
+        center: new google.maps.LatLng(-8.062491, -34.872888),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
     var map = new google.maps.Map(mapCanvas,mapOptions);
-    
-    
+
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
